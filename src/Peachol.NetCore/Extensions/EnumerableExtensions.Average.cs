09fc4ff0f7ce650e8fbbb8d3b66db115033f783f
HUMAOSHUAI <hu15150@126.com>
2024-09-05 10:13:07 +0800

    average default value

---
+++ src/Peachol.NetCore/Extensions/EnumerableExtensions.Average.cs
@@ -0,0 +1,64 @@
+﻿namespace System.Collections.Generic;
+
+public static partial class EnumerableExtensions
+{
+    public static double Average<TSource>(this IEnumerable<TSource> source, Func<TSource, int> selector, int defaultValue)
+        => source.Any() ? source.Average(selector) : defaultValue;
+
+    public static double Average<TSource>(this IEnumerable<TSource> source, Func<TSource, long> selector, long defaultValue)
+        => source.Any() ? source.Average(selector) : defaultValue;
+
+    public static decimal? Average<TSource>(this IEnumerable<TSource> source, Func<TSource, decimal?> selector, decimal? defaultValue)
+        => source.Any() ? source.Average(selector) : defaultValue;
+
+    public static float Average<TSource>(this IEnumerable<TSource> source, Func<TSource, float> selector, float defaultValue)
+        => source.Any() ? source.Average(selector) : defaultValue;
+
+    public static double? Average<TSource>(this IEnumerable<TSource> source, Func<TSource, int?> selector, int? defaultValue)
+        => source.Any() ? source.Average(selector) : defaultValue;
+
+    public static float? Average<TSource>(this IEnumerable<TSource> source, Func<TSource, float?> selector, float? defaultValue)
+        => source.Any() ? source.Average(selector) : defaultValue;
+
+    public static double Average<TSource>(this IEnumerable<TSource> source, Func<TSource, double> selector, double defaultValue)
+        => source.Any() ? source.Average(selector) : defaultValue;
+
+    public static double? Average<TSource>(this IEnumerable<TSource> source, Func<TSource, double?> selector, double? defaultValue)
+        => source.Any() ? source.Average(selector) : defaultValue;
+
+    public static decimal Average<TSource>(this IEnumerable<TSource> source, Func<TSource, decimal> selector, decimal defaultValue)
+        => source.Any() ? source.Average(selector) : defaultValue;
+
+    public static double? Average<TSource>(this IEnumerable<TSource> source, Func<TSource, long?> selector, long? defaultValue)
+        => source.Any() ? source.Average(selector) : defaultValue;
+
+    public static float? Average(this IEnumerable<float?> source, float? defaultValue)
+        => source.Any() ? source.Average() : defaultValue;
+
+    public static decimal Average(this IEnumerable<decimal> source, decimal defaultValue)
+        => source.Any() ? source.Average() : defaultValue;
+
+    public static double Average(this IEnumerable<int> source, double defaultValue)
+        => source.Any() ? source.Average() : defaultValue;
+
+    public static double Average(this IEnumerable<long> source, double defaultValue)
+        => source.Any() ? source.Average() : defaultValue;
+
+    public static double Average(this IEnumerable<double> source, double defaultValue)
+        => source.Any() ? source.Average() : defaultValue;
+
+    public static double? Average(this IEnumerable<double?> source, double? defaultValue)
+        => source.Any() ? source.Average() : defaultValue;
+
+    public static double? Average(this IEnumerable<int?> source, double? defaultValue)
+        => source.Any() ? source.Average() : defaultValue;
+
+    public static double? Average(this IEnumerable<long?> source, double? defaultValue)
+        => source.Any() ? source.Average() : defaultValue;
+
+    public static decimal? Average(this IEnumerable<decimal?> source, decimal? defaultValue)
+        => source.Any() ? source.Average() : defaultValue;
+
+    public static float Average(this IEnumerable<float> source, float defaultValue)
+        => source.Any() ? source.Average() : defaultValue;
+}
