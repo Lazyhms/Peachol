f42faa6f1f0848cd86c724ad9af34d8d06c19127
HUMAOSHUAI <hu15150@126.com>
2024-11-14 15:52:17 +0800

    add new

--- src/Peachol.NetCore/Extensions/EnumerableExtensions.cs
+++ src/Peachol.NetCore/Extensions/EnumerableExtensions.cs
@@ -1,5 +1,4 @@
-﻿
-namespace System.Collections.Generic;
+﻿namespace System.Collections.Generic;
 
 public static partial class EnumerableExtensions
 {
@@ -17,6 +16,7 @@ public static partial class EnumerableExtensions
         foreach (var item in source)
         {
             checked { index++; }
+
             action(item, index);
         }
     }
@@ -29,6 +29,46 @@ public static partial class EnumerableExtensions
 
     public static string Join(this IEnumerable<string?> values, string? separator)
         => string.Join(separator, values);
+
+    public static IEnumerable<TSource> Insert<TSource>(this IEnumerable<TSource> source, int index, TSource item)
+    {
+        using IEnumerator<TSource> e = source.GetEnumerator();
+
+        int tempIndex = -1;
+        while (e.MoveNext())
+        {
+            checked { tempIndex++; }
+
+            if (tempIndex == index)
+            {
+                yield return item;
+            }
+
+            yield return e.Current;
+        }
+    }
+
+    public static IEnumerable<TSource> Insert<TSource>(this IEnumerable<TSource> source, int index, IEnumerable<TSource> items)
+    {
+        using IEnumerator<TSource> e = source.GetEnumerator();
+
+        int tempIndex = -1;
+        while (e.MoveNext())
+        {
+            checked { tempIndex++; }
+
+            if (tempIndex == index)
+            {
+                using IEnumerator<TSource> e1 = items.GetEnumerator();
+                while (e1.MoveNext())
+                {
+                    yield return e1.Current;
+                }
+            }
+
+            yield return e.Current;
+        }
+    }
 
     public static IEnumerable<TResult> LeftJoin<TOuter, TInner, TKey, TResult>(
         this IQueryable<TOuter> outer,
