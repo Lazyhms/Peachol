9c84b2d966483d2192e568bbb5103c720be79f77
HUMAOSHUAI <hu15150@126.com>
2025-01-21 11:22:49 +0800

    update nuget

--- src/EFCore/Extensions/EntityFrameworkCoreQueryableExtensions.cs
+++ src/EFCore/Extensions/EntityFrameworkCoreQueryableExtensions.cs
@@ -1,17 +1,77 @@
-﻿namespace Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore.Query.Internal;
+
+namespace Microsoft.EntityFrameworkCore;
 
 public static class EntityFrameworkCoreQueryableExtensions
 {
     public static IOrderedQueryable<TEntity> OrderBy<TEntity>(this IQueryable<TEntity> source, string propertyOrFieldName) where TEntity : class
-        => source.OrderBy(o => EF.Property<TEntity>(o, propertyOrFieldName));
+    {
+        var parameterExpression = Expression.Parameter(typeof(TEntity));
+        var memberExpression = Expression.PropertyOrField(parameterExpression, propertyOrFieldName);
+
+        return (IOrderedQueryable<TEntity>)
+            (source.Provider is EntityQueryProvider
+                ? source.Provider.CreateQuery<TEntity>(
+                    Expression.Call(
+                        null,
+                        QueryableMethods.OrderBy.MakeGenericMethod(parameterExpression.Type, memberExpression.Type),
+                        [source.Expression, Expression.Lambda(memberExpression, parameterExpression)]))
+                : source);
+    }
 
     public static IOrderedQueryable<TEntity> OrderByDescending<TEntity>(this IQueryable<TEntity> source, string propertyOrFieldName) where TEntity : class
-        => source.OrderByDescending(o => EF.Property<TEntity>(o, propertyOrFieldName));
+    {
+        var parameterExpression = Expression.Parameter(typeof(TEntity));
+        var memberExpression = Expression.PropertyOrField(parameterExpression, propertyOrFieldName);
+
+        return (IOrderedQueryable<TEntity>)
+            (source.Provider is EntityQueryProvider
+                ? source.Provider.CreateQuery<TEntity>(
+                    Expression.Call(
+                        null,
+                        QueryableMethods.OrderByDescending.MakeGenericMethod(parameterExpression.Type, memberExpression.Type),
+                        [source.Expression, Expression.Lambda(memberExpression, parameterExpression)]))
+                 : source);
+    }
 
     public static IOrderedQueryable<TEntity> ThenBy<TEntity>(this IOrderedQueryable<TEntity> source, string propertyOrFieldName) where TEntity : class
-        => source.ThenBy(o => EF.Property<TEntity>(o, propertyOrFieldName));
+    {
+        var parameterExpression = Expression.Parameter(typeof(TEntity));
+        var memberExpression = Expression.PropertyOrField(parameterExpression, propertyOrFieldName);
+
+        return (IOrderedQueryable<TEntity>)
+            (source.Provider is EntityQueryProvider
+                ? source.Provider.CreateQuery<TEntity>(
+                    Expression.Call(null,
+                    QueryableMethods.ThenBy.MakeGenericMethod(parameterExpression.Type, memberExpression.Type),
+                    [source.Expression, Expression.Lambda(memberExpression, parameterExpression)]))
+                : source);
+    }
 
     public static IOrderedQueryable<TEntity> ThenByDescending<TEntity>(this IOrderedQueryable<TEntity> source, string propertyOrFieldName) where TEntity : class
+    {
+        var parameterExpression = Expression.Parameter(typeof(TEntity));
+        var memberExpression = Expression.PropertyOrField(parameterExpression, propertyOrFieldName);
+
+        return (IOrderedQueryable<TEntity>)
+            (source.Provider is EntityQueryProvider
+                ? source.Provider.CreateQuery<TEntity>(
+                    Expression.Call(null,
+                    QueryableMethods.ThenByDescending.MakeGenericMethod(parameterExpression.Type, memberExpression.Type),
+                    [source.Expression, Expression.Lambda(memberExpression, parameterExpression)]))
+                : source);
+    }
+
+    public static IOrderedQueryable<TEntity> OrderByEFProperty<TEntity>(this IQueryable<TEntity> source, string propertyOrFieldName) where TEntity : class
+        => source.OrderBy(o => EF.Property<TEntity>(o, propertyOrFieldName));
+
+    public static IOrderedQueryable<TEntity> OrderByEFPropertyDescending<TEntity>(this IQueryable<TEntity> source, string propertyOrFieldName) where TEntity : class
+        => source.OrderByDescending(o => EF.Property<TEntity>(o, propertyOrFieldName));
+
+    public static IOrderedQueryable<TEntity> ThenByEFProperty<TEntity>(this IOrderedQueryable<TEntity> source, string propertyOrFieldName) where TEntity : class
+        => source.ThenBy(o => EF.Property<TEntity>(o, propertyOrFieldName));
+
+    public static IOrderedQueryable<TEntity> ThenByEFPropertyDescending<TEntity>(this IOrderedQueryable<TEntity> source, string propertyOrFieldName) where TEntity : class
         => source.ThenByDescending(o => EF.Property<TEntity>(o, propertyOrFieldName));
 
     public static Pagination<TEntity> Pagination<TEntity>(this IQueryable<TEntity> source, int pageIndex, int pageSize)
