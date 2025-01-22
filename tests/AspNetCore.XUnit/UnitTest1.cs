c9db6e7bb65faef3b1f5f8685589510fb68decc1
HUMAOSHUAI <hu15150@126.com>
2025-01-22 18:08:27 +0800

    up

--- tests/AspNetCore.XUnit/UnitTest1.cs
+++ tests/AspNetCore.XUnit/UnitTest1.cs
@@ -2,6 +2,7 @@ using System.Linq.Expressions;
 using System.Text.Json;
 using System.Text.Json.Nodes;
 using System.Text.Json.Serialization;
+using Xunit;
 
 namespace AspNetCore.XUnit;
 
--- tests/EF.xUnit/DbSet.Tests.cs
+++ tests/EF.xUnit/DbSet.Tests.cs
@@ -1,5 +1,6 @@
 ﻿using System.Data;
 using Microsoft.EntityFrameworkCore;
+using Xunit;
 
 namespace EF.XUnit;
 
